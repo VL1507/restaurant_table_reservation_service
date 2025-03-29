0af9f23b23c9f7b30ddbcef533b65e5d0fdb9b35
Vladimir Mashyanov <vova.mashyanov@gmail.com>
2025-03-30 00:48:54 +1000

    ..

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,13 +1,13 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker
+# from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from app.database.db1.base import BaseDB1, engin_db1
-from app.database.db2.base import BaseDB2, engin_db2
+# from app.database.db1.base import BaseDB1, engin_db1
+# from app.database.db2.base import BaseDB2, engin_db2
 
-async_session = async_sessionmaker(
-    binds={BaseDB1: engin_db1, BaseDB2: engin_db2}, expire_on_commit=False
-)
+# async_session = async_sessionmaker(
+#     binds={BaseDB1: engin_db1, BaseDB2: engin_db2}, expire_on_commit=False
+# )
 
 
-async def get_session():
-    async with async_session() as session:
-        return session
+# async def get_session():
+#     async with async_session() as session:
+#         return session
--- backend/app/database/db1/tables.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import VARCHAR, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
-
-from app.database.mixins import IntegerIDMixin, TimestampMixin
-
-from .base import BaseDB1
